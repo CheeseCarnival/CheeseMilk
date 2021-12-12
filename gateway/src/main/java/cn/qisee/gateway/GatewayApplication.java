0b8d14579a4c7a3146e800807f7f3c94ca4840e2
xxxcrel <crelxc@gmail.com>
2021-12-12 16:44:15 +0800

    update

--- gateway/src/main/java/cn/qisee/gateway/GatewayApplication.java
+++ gateway/src/main/java/cn/qisee/gateway/GatewayApplication.java
@@ -1,6 +1,7 @@
 package cn.qisee.gateway;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -11,11 +12,9 @@ import org.springframework.session.data.redis.config.annotation.web.server.Enabl
 @EnableRedisWebSession
 public class GatewayApplication {
     public static void main(String[] args) {
-
+        new SpringApplicationBuilder()
+                .sources(GatewayApplication.class)
+                .run(args);
     }
 
-    @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
-    }
 }
